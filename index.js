f25051d451180be3aea3448c181db80756ad2682
Nicholas Valletta <inchworm333@gmail.com>
2021-01-12 04:40:34 -0500

    Made multiple improvements mainly involving js template literals + tagged templates

--- index.js
+++ index.js
@@ -9,8 +9,13 @@ const events = {
 	MESSAGE_REACTION_ADD: 'messageReactionAdd',
 	MESSAGE_REACTION_REMOVE: 'messageReactionRemove',
 }
+
+// Lists
 let createdChannels = [];
 let online_mentor_afk_list = []
+
+// Mentoring Variables
+const mentoring_times = "10 am - 6 pm Monday through Friday";
 
 // Set the bot's presence (activity and status)
 client.on("ready", () => {
@@ -39,13 +44,13 @@ client.on('message', async msg => {
 	if (msg.author.bot) return;
 
 	// Load the roles active on the server
-	mentor_role = msg.guild.roles.cache.find(role => role.name === "Mentor")
-	online_role = msg.guild.roles.cache.find(role => role.name === "Online Mentor")
-	social_role = msg.guild.roles.cache.find(role => role.name === "Social")
+	let mentor_role = msg.guild.roles.cache.find(role => role.name === "Mentor")
+	let online_role = msg.guild.roles.cache.find(role => role.name === "Online Mentor")
+	let social_role = msg.guild.roles.cache.find(role => role.name === "Social")
 
 	// Get member roles
-	mentor = false
-	online = false
+	let mentor = false;
+	let online = false;
 	msg.member.roles.cache.forEach((key, value) => {
 		if (value === mentor_role.id) {
 			mentor = true;
@@ -56,7 +61,7 @@ client.on('message', async msg => {
 
 	// See if a mentor returned from being afk
 	if (online && online_mentor_afk_list.length) {
-		index = online_mentor_afk_list.findIndex((afk_mentor) => msg.author === afk_mentor.name)
+		let index = online_mentor_afk_list.findIndex((afk_mentor) => msg.author === afk_mentor.name)
 		if(index !== -1) {
 			online_mentor_afk_list.splice(index, 1)
 			msg.channel.send(`${msg.author} has returned to their keyboard. They will be right with you :hugging::raised_hands:`)
@@ -65,41 +70,44 @@ client.on('message', async msg => {
 
 	//Commands that can be run at any time outside of mentoring hours
 	if (msg.content.toLowerCase().startsWith("!help")) {
-		mentor_cmds = ""
+		let mentor_cmds;
 		if (mentor) {
-			mentor_cmds = "\n```" +
-				"\nMentor-Only commands:" +
-				"```" +
-				"\n!online -> Sets your status so you appear as the current mentor on duty" +
-				"\n!offline -> Removes your status as the current mentor on duty" +
-				"\n!brb {optional: [minutes until return]} -> Notifies any mentees that use !ping that you are afk and returning soon" +
-				"\n!sos -> In case of emergency, request help from another mentor" +
-				"\n!close -> Removes all existing voice and text channels" +
-				"\nNote: All commands work for you 24/7. Before 10 and after 6 mentees can't run commands"
+			mentor_cmds = `
+				Mentor-Only commands:
+				${quote`
+					${inline`!online`} -> Sets your status so you appear as the current mentor on duty
+					${inline`!offline`} -> Removes your status as the current mentor on duty
+					${inline`!brb`} {optional: [minutes until return]} -> Notifies any mentees that use !ping that you are afk and returning soon
+					${inline`!sos`} -> In case of emergency, request help from another mentor
+					${inline`!close`} -> Removes all existing voice and text channels
+					Note: All commands work for you 24/7. Before 10 and after 6 mentees can't run commands
+				`}
+			`
 		}
 
-		msg.channel.send(`Hi ${msg.author}, welcome to the eSSE's mentoring system! We're here to help.` +
-			"\nHere's a few helpful commands:" +
-			"\n```" +
-			"\n!help -> See this command (but you knew that already)" +
-			"\n!ping -> Make mentors aware you need help (Please use discretion, there may only be one mentor online at a time)" +
-			"\n!join -> Enters you in private voice and chat channels to speak one-on-one with a mentor" +
-			"\n!social -> Allows you to see our discord social chats! Anyone can chat at any time and its way less serious than the rest of the mentoring discord" +
-			mentor_cmds +
-			"\n```" +
-			"\nPlease remember the following items:" +
-			"\n```" +
-			"\nWe are volunteers (we don't get paid)" +
-			"\nOur hours are 10 am - 6 pm Monday through Friday" +
-			"\nWe are an official RIT organization. PLEASE no profanity, harrassment, sexual comments, or anything else made to mentors or other mentees" +
-			"\nIf you are overly aggressive to our mentors or break any of the rules above, you will be banned permanently and you will be reported to RIT" +
-			"\n```" +
-			"\n*If you would like to mute this channel to prevent being spammed with notifications, right click on the channel in the navigation bar to the " + 
-			"left, navigate to \"Notifications\" and select \"Only @mentions\"*"
+		msg.channel.send(`
+			Hi ${msg.author}, welcome to the eSSE's mentoring system! We're here to help.
+			Here's a few helpful commands:
+			${quote`
+				${inline`!help`} -> See this command (but you knew that already)
+				${inline`!ping`} -> Make mentors aware you need help (Please use discretion, there may only be one mentor online at a time)
+				${inline`!join`} -> Enters you in private voice and chat channels to speak one-on-one with a mentor
+				${inline`!social`} -> Allows you to see our discord social chats! Anyone can chat at any time and its way less serious than the rest of the mentoring discord
+			`}
+			${mentor_cmds}
+			Please remember the following items:
+			${quote`
+				We are volunteers (we don't get paid)
+				Our hours are ${mentoring_times}
+				We are an official RIT organization. PLEASE no profanity, harassment, sexual comments, or anything else made to mentors or other mentees
+				If you are overly aggressive to our mentors or break any of the rules above, you will be banned permanently and you will be reported to RIT
+			`}
+			*If you would like to mute this channel to prevent being spammed with notifications, right click on the channel in the navigation bar to the left, navigate to "Notifications" and select "Only @mentions"*
+			`
 		)
 		return
 	} else if (msg.content.toLowerCase().startsWith("!social")) {
-		member = msg.member
+		let member = msg.member
 		if (member.roles.cache.has(social_role.id)) {
 			msg.react('💩')
 		} else {
@@ -111,11 +119,12 @@ client.on('message', async msg => {
 
 	// Prevent commands below this from being run outside of mentoring hours
 	let now = new Date();
-	if (now.getHours() < 9 || now.getHours() > 17 || now.getDay() == 0 || now.getDay() == 6) {
+	if (now.getHours() < 9 || now.getHours() > 17 || now.getDay() === 0 || now.getDay() === 6) {
 		if (msg.content.toLowerCase().startsWith("!") && !mentor) {
-			msg.channel.send(`Sorry ${msg.author}, but we currently only offer online mentoring from 10AM-6PM on Monday through Friday. ` +
-				"If you have just a quick question feel free to post it in " +
-				"<#691857971675791370> and someone might be able to help!")
+			msg.channel.send(`
+				Sorry ${msg.author}, but we currently only offer online mentoring from ${mentoring_times}.
+				If you have just a quick question feel free to post it in
+				<#691857971675791370> and someone might be able to help!`)
 			return
 		}
 	}
@@ -212,59 +221,101 @@ client.on('message', async msg => {
 
 	// Mentor specific commands
 	if (mentor) {
-		if (msg.content.toLowerCase().startsWith("!close")) {
-			msg.channel.send(`${msg.author}, shutting down all voice channels`)
-			createdChannels.forEach((channel) => {
-				channel.children.forEach((childChannel) => {
-					childChannel.delete("closing time *Insert song here*")
-				})
-				channel.delete("closing time *Insert song here*")
-			})
-			createdChannels = []
-		} else if (msg.content.toLowerCase().startsWith("!sos")) {
-			msg.channel.send(`${msg.author} is in need of assistance. Would any ${mentor_role} like to volunteer as tribute to assist this hard working individual? Please, you are our only hope`, {files: ["./SOS.png"]})
-		} else if (msg.content.toLowerCase().startsWith("!online")) {
-			if (!online) {
-				msg.member.roles.add(online_role)
-				msg.channel.send(`${msg.author} is now mentoring`)
-			} else {
-				msg.channel.send(`${msg.author} is already online`)
-			}
-		} else if (msg.content.toLowerCase().startsWith("!offline")) {
-			if (online) {
-				msg.member.roles.remove(online_role)
-				msg.channel.send(`${msg.author} is no longer mentoring`)
-			} else {
-				msg.channel.send(`${msg.author} isn't currently mentoring to begin with`)
-			}
-		} else if (msg.content.toLowerCase().startsWith("!brb")) {
-			if (online) {
-				let estimated_return_time = ""
-				cmds = msg.content.split(" ")
-				if (cmds.length != 1) {
-					now.setMinutes(now.getMinutes() + parseInt(cmds[1]))
-					estimated_return_time = now.getHours() + ":" + now.getMinutes()
-				}
+		switch (msg.content.split(' ')[0]) {
 
-				online_mentor_afk_list.push({name: msg.author, estimated_return_time: estimated_return_time})
-				msg.channel.send(`${msg.author} will be right back. Keep up the good work and don't miss them too much :)`)
-			}
-		} else if (msg.content.toLowerCase().startsWith("!trash30")) {
-			if (online) {
-				msg.channel.send("Heeeeeeeeeey Lab! It's Trash:30. Throw out any trash around you, even if it's not yours.")
-			} else {
-				msg.channel.send(`${msg.author} isn't the mentor on duty. Don't fall for their shenanigans.`)
-			}
-		} else if (msg.content.toLowerCase().startsWith("!morning")) {
-			msg.channel.send("Good morning everybody! Hope you are ready for another busy day of mentoring :smile:",
-				{files: ["https://www.poynter.org/wp-content/uploads/2019/07/shutterstock_264132746.jpg"]}
-			);
-		} else if (msg.content.toLowerCase().startsWith("!nightnight")) {
-			msg.channel.send("The SSE Mentors are getting a good night rest so they can help you out bright and early tomorrow morning.",
-				{files: ["https://media.tenor.com/images/950960797e3b597b8fedc869eabad846/tenor.gif"]}
-			);
+			case "!close":
+				msg.channel.send(`${msg.author}, shutting down all voice channels`)
+				createdChannels.forEach((channel) => {
+					channel.children.forEach((childChannel) => {
+						childChannel.delete("closing time *Insert song here*")
+					})
+					channel.delete("closing time *Insert song here*")
+				})
+				createdChannels = []
+				break;
+
+			case "!sos":
+				msg.channel.send(`${msg.author} is in need of assistance. Would any ${mentor_role} like to volunteer as tribute to assist this hard working individual? Please, you are our only hope`, {files: ["./SOS.png"]})
+				break;
+
+			case "!online":
+				if (!online) {
+					msg.member.roles.add(online_role)
+					msg.channel.send(`${msg.author} is now mentoring`)
+				} else {
+					msg.channel.send(`${msg.author} is already online`)
+				}
+				break;
+
+			case "!offline":
+				if (online) {
+					msg.member.roles.remove(online_role)
+					msg.channel.send(`${msg.author} is no longer mentoring`)
+				} else {
+					msg.channel.send(`${msg.author} isn't currently mentoring to begin with`)
+				}
+				break;
+
+			case "!brb":
+				if (online) {
+					let estimated_return_time = ""
+					cmds = msg.content.split(" ")
+					if (cmds.length != 1) {
+						now.setMinutes(now.getMinutes() + parseInt(cmds[1]))
+						estimated_return_time = now.getHours() + ":" + now.getMinutes()
+					}
+
+					online_mentor_afk_list.push({name: msg.author, estimated_return_time: estimated_return_time})
+					msg.channel.send(`${msg.author} will be right back. Keep up the good work and don't miss them too much :)`)
+				}
+				break;
+
+			case "!trash30":
+				if (online) {
+					msg.channel.send("Heeeeeeeeeey Lab! It's Trash:30. Throw out any trash around you, even if it's not yours.")
+				} else {
+					msg.channel.send(`${msg.author} isn't the mentor on duty. Don't fall for their shenanigans.`)
+				}
+				break;
+
+			case "!morning":
+				msg.channel.send("Good morning everybody! Hope you are ready for another busy day of mentoring :smile:",
+					{files: ["https://www.poynter.org/wp-content/uploads/2019/07/shutterstock_264132746.jpg"]}
+				);
+				break;
+
+			case "!nightnight":
+				msg.channel.send("The SSE Mentors are getting a good night rest so they can help you out bright and early tomorrow morning.",
+					{files: ["https://media.tenor.com/images/950960797e3b597b8fedc869eabad846/tenor.gif"]}
+				);
+				break;
 		}
 	}
 })
 
 client.login(process.env.BOT_TOKEN);
+
+
+//////////////////////////////////////
+////// Discord Markdown Helpers //////
+//////////////////////////////////////
+
+// From: 'example text'
+// To: '`example text`'
+let inline = (strings) => {
+	return "`" + strings + "`";
+}
+
+// From: 'example text
+//        example text
+//        example text'
+// To:   '>example text
+//        >example text
+//        >example text'
+let quote = (strings) => {
+	let response = '';
+	strings.split('\n').forEach( (string) => {
+		response += `> ${string}\n`;
+	});
+	return response;
+}
